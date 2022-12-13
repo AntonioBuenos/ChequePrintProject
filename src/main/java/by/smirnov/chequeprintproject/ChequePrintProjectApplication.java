05b88683ecae15c5061c5aca56c1649534bddabd
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-12-13 22:46:32 +0300

    Initial Commit

---
+++ src/main/java/by/smirnov/chequeprintproject/ChequePrintProjectApplication.java
@@ -0,0 +1,13 @@
+package by.smirnov.chequeprintproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChequePrintProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChequePrintProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/smirnov/chequeprintproject/ChequePrintProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.smirnov.chequeprintproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChequePrintProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
