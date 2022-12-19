2155196583085f8ae4d32fa1ced2a7708990b57e
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-12-19 19:29:08 +0300

    ChequeRunner & InputHandler refactored

--- src/main/java/by/smirnov/chequeprintproject/parser/InputHandler.java
+++ src/main/java/by/smirnov/chequeprintproject/parser/InputHandler.java
@@ -5,36 +5,60 @@ import by.smirnov.chequeprintproject.domain.DiscountCard;
 import by.smirnov.chequeprintproject.domain.Product;
 import by.smirnov.chequeprintproject.domain.Store;
 import by.smirnov.chequeprintproject.repository.DiscountCardRepository;
+import by.smirnov.chequeprintproject.repository.DiscountCardSetRepositoryImpl;
 import by.smirnov.chequeprintproject.repository.ProductRepository;
+import by.smirnov.chequeprintproject.repository.ProductSetRepositoryImpl;
 import by.smirnov.chequeprintproject.service.chequebuilder.ChequeCounter;
 import by.smirnov.chequeprintproject.service.chequebuilder.StringChequeBuilder;
-import lombok.RequiredArgsConstructor;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@RequiredArgsConstructor
 public class InputHandler {
 
+    public static final String DIV = "-";
+    public static final String DIV_SPACE = " ";
     private final ProductRepository repository;
     private final DiscountCardRepository cardRepository;
 
-    public void handleCheque(String input){
-        handleCheque(input.split(" "));
+    public InputHandler() {
+        repository = new ProductSetRepositoryImpl();
+        cardRepository = new DiscountCardSetRepositoryImpl();
     }
 
-    public void handleCheque(String[] params){
+    public void processCheque(String[] args) {
+        if (args[0].equalsIgnoreCase("java")) {
+            if (args[1].equalsIgnoreCase("CheckRunner")) {
+                handleCheque(args);
+            } else if (args[1].equalsIgnoreCase("file")) {
+                List<String> lines = InputFileReader.readFile(args[2]);
+                for (String line : lines) {
+                    handleCheque(line);
+                }
+            }
+        }
+    }
+
+    public void handleCheque(String input) {
+        handleCheque(input.split(DIV_SPACE));
+    }
+
+    public void handleCheque(String[] params) {
         Map<Product, Integer> productCart = new HashMap<>();
         for (int i = 2; i < params.length - 2; i++) {
-            String[] productLine = params[i].split("-");
+            String[] productLine = params[i].split(DIV);
             productCart.put(repository.findById(Long.parseLong(productLine[0])), Integer.parseInt(productLine[1]));
         }
-        String[] cardline = params[params.length - 2].split("-");
-        DiscountCard card = cardRepository.findById(Long.parseLong(cardline[1]));
-        String[] cashierline = params[params.length - 1].split("-");
-        Cashier cashier = new Cashier(Long.parseLong(cashierline[1]), Store.SHOP);
-        ChequeCounter chequeCounter = new ChequeCounter(productCart, card);
-        StringChequeBuilder chequeBuilder = new StringChequeBuilder(chequeCounter);
+        DiscountCard card = cardRepository.findById(getValue(params[params.length - 2]));
+        Cashier cashier = new Cashier(getValue(params[params.length - 1]), Store.SHOP);
+        StringChequeBuilder chequeBuilder = new StringChequeBuilder(new ChequeCounter(productCart, card));
         chequeBuilder.print(cashier);
     }
+
+    private Long getValue(String param) {
+        String[] keyAdValue = param.split(DIV);
+        return Long.parseLong(keyAdValue[1]);
+    }
+
 }
