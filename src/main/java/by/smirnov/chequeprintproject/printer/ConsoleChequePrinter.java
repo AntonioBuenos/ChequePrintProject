5c1d246c2b4cbc0db8595987dcca23e5d372c147
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-08 16:40:38 +0300

    Migrate project from Maven to Gradle

--- src/main/java/by/smirnov/chequeprintproject/printer/ConsoleChequePrinter.java
+++ src/main/java/by/smirnov/chequeprintproject/printer/ConsoleChequePrinter.java
@@ -1,8 +1,11 @@
 package by.smirnov.chequeprintproject.printer;
 
 public class ConsoleChequePrinter extends ChequePrinter{
+
+
     @Override
     public void print(CharSequence cheque) {
+
         System.out.println(cheque);
     }
 }
